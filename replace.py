cb47143c364cf599167c09a731060aba20de41b6
Copper76 <gghhgh112212@gmail.com>
2021-02-13 14:13:05 +0000

    Added an alternative function
    The other function takes a threshold and returns a value to see if the threshold is exceeded

--- replace.py
+++ replace.py
@@ -7,17 +7,30 @@ def calc_dist(string1, string2):
     elif string1[0] == string2[0]:
         val += calc_dist(string1[1:], string2[1:])
     else:
-        val = 1 + min([calc_dist(string1, string2[1:]),calc_dist(string1[1:], string2),calc_dist(string1[1:], string2[1:])])
+        val = 1 + min(
+            [calc_dist(string1, string2[1:]), calc_dist(string1[1:], string2), calc_dist(string1[1:], string2[1:])])
     return val
 
-def rep_words(inputString,keyword_list,threshold):
+def calc_dist_bool(string1, string2, threshhold):
+    if string1 == "":
+        return len(string2) < threshold
+    elif string2 == "":
+        return len(string1) < threshold
+    elif string1[0] == string2[0]:
+        return calc_dist(string1[1:], string2[1:]) < threshold
+    else:
+        return 1 + min(
+            [calc_dist(string1, string2[1:]), calc_dist(string1[1:], string2), calc_dist(string1[1:], string2[1:])]) < threshhold
+
+
+def rep_words(inputString, keyword_list, threshold):
     final = ""
     words = inputString.split()
     keywords = keyword_list.split(",")
     for word in words:
         for keyword in keywords:
-            if calc_dist(word,keyword) < threshold:
-                final += " "+keyword
+            if calc_dist(word, keyword) < threshold:
+                final += " " + keyword
             else:
                 final += " " + word
     return final
