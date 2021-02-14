2039ab822a9f4ababade2ca23684b00981cc5a56
devkanani25 <devkanani25@gmail.com>
2021-02-14 11:41:25 +0000

    modified:   crucialcrucialwords.txt 	new file:   dw_villains_and_other_things.txt 	modified:   getCrucialWords.py 	modified:   quotegenerator.css 	modified:   quotegenerator.html

--- getCrucialWords.py
+++ getCrucialWords.py
@@ -1,8 +1,22 @@
 from collections import Counter
 
+punctuation = [",",".","!","?",";",":","[","]","(",")"]
+
 with open('dwscripts.txt') as fin:
-    counter = Counter(fin.read().strip().split())
+    
+    x = fin.read().strip().lower()
+    
+    for p in punctuation:
+        x = x.replace(p,"")
+    
+    counter = Counter(x.split())
 
 f = open("crucialcrucialwords.txt","w")
-f.write(str(counter.most_common()))
+
+crucialwords = []
+
+for word, count in counter.most_common(1000):
+    crucialwords.append(word + "")
+
+f.write(str(crucialwords))
 f.close()
