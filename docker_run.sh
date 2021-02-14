0833d00783145138f0e998bb8696802cd79e492d
Dulhan Jayalath <dhjayalath@protonmail.com>
2021-02-14 10:58:26 +0000

    add some more crap

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,4 @@
 
 sudo docker build -t custom-gpu-tf .
 #sudo docker run -e DISPLAY=$DISPLAY -v $(pwd)/:/home -u $(id -u):$(id -g) --runtime=nvidia -it custom-gpu-tf bash
-sudo docker run -p 8888:8888 -v $(pwd)/:/home -u $(id -u):$(id -g) --runtime=nvidia -it custom-gpu-tf bash
+sudo docker run --memory=32g -p 8888:8888 -v $(pwd)/:/home -u $(id -u):$(id -g) --runtime=nvidia -it custom-gpu-tf bash
